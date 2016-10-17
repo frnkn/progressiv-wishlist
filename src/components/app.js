a29d3a66dab20a0e473386552bae09e47ae476d3
Carl Bednorz <carl@frnkn.com>
2016-10-17 23:20:29 +0200

    1st component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Wishlist from './wishlist';
 
 export default class App extends React.Component {
   render(){
     return(
       <div>
         <h1>Hello World</h1>
+        <Wishlist />
       </div>
     )
   }
