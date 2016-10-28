9655c5c2b0b3727b15c21cda2bb7484fb2e828ca
Carl <carl@frnkn.com>
2016-10-28 18:52:03 +0200

    some refactoring

--- src/components/app.js
+++ src/components/app.js
@@ -3,20 +3,21 @@ import Wishlist from './wishlist';
 
 const products = [
   {product_id: 123, name: "Jeans", price: "12.23"},
-  {product_id: 123, name: "Hose", price: "12.23"},
-  {product_id: 123, name: "Pullover", price: "12.23"},
-  {product_id: 123, name: "Hemd", price: "12.23"}
+  {product_id: 1234, name: "Hose", price: "12.23"},
+  {product_id: 1235, name: "Pullover", price: "12.23"},
+  {product_id: 1236, name: "Hemd", price: "12.23"}
 ];
 
 export default class App extends React.Component {
 
-
+  
 
   render(){
     return(
       <div>
         <h1>A Progressive Wishlist App Based On React</h1>
-        <Wishlist data={products}/>
+        <button className="button button-primary">Add Dummy Product</button>
+        <Wishlist products={products}/>
       </div>
     );
   }
--- src/components/wishlist.js
+++ src/components/wishlist.js
@@ -5,16 +5,16 @@ import WishlistItem from './wishlist-item.js';
 export default class Wishlist extends React.Component {
   constructor(props) {
     super(props);
-    this.state = props;
-    console.log(props.data);
-    this.addDummyProduct = this.addDummyProduct.bind(this);
+    //this.state = props.products;
+    console.log("PROPS", props.products);
+    console.log("STATE", this.state);
   }
-
+  /* Not used - Directly set in {} within render function
   renderWishlistItems() {
     let all_items = [];
     var idx = 0;
-    for(let product of this.state.data){
-
+    for(let product of this.props.products){
+      console.log("PUSH");
       all_items.push(
         <WishlistItem key={idx} product_id={product.product_id} name={product.name} price={product.price} />
       );
@@ -22,31 +22,12 @@ export default class Wishlist extends React.Component {
     }
     return all_items;
 
-  }
+  }*/
 
-  addWishlistItem() {
-    let product = {
-      product_id: "23",
-      name: "New Product",
-      price: "599.99"
-    };
-  }
-
-  addDummyProduct() {
-    let product = {
-      product_id: "23",
-      name: "New Product",
-      price: "599.99"
-    };
-    console.log("Add Dummy Product");
-    this.setState.products = {'hello': 'world'};
-    console.log("sTATE", this.state);
-  }
 
   render() {
     return (
       <div>
-        <button onClick={this.addDummyProduct} className="button button-primary">Add Dummy Product</button>
         <table className="u-full-width">
           <thead>
             <tr>
@@ -57,7 +38,10 @@ export default class Wishlist extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {this.renderWishlistItems()}
+            {this.props.products.map(product =>(
+              <WishlistItem key={product.product_id} product_id={product.product_id} name={product.name} price={product.price} />
+            ))}
+
           </tbody>
         </table>
       </div>
